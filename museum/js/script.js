f038cda385a3d77bdc388c8650647916f3c08f3e
Alexander Kharmansky <davilex_x@vk.com>
2021-09-26 17:21:28 +0300

    feat: add links in visiting section, add video section

---
+++ museum/js/script.js
@@ -0,0 +1,13 @@
+const progressVideo = document.querySelector('.video__range');
+  
+progressVideo.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+})
+ 
+const progressVolume = document.querySelector('.video__volume-range');
+  
+progressVolume.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+})
